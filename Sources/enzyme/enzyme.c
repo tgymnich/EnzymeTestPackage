49e917a9b94f98f0d2972ff35d3bc63008e135a3
Tim Gymnich <tgymnich@icloud.com>
2021-07-03 01:36:35 +0200

    Update enzyme.c

--- Sources/enzyme/enzyme.c
+++ Sources/enzyme/enzyme.c
@@ -1,2 +1,30 @@
+#include <enzyme.h>
+#include <stdio.h>
 
+double square(double x) {
+    return x * x;
+}
+double dsquare(double x) {
+    // This returns the derivative of square or 2 * x
+    return __enzyme_autodiff((void*)square, x);
+}
 
+void run() {
+    for(double i=1; i<5; i++)
+        printf("square(%f)=%f, dsquare(%f)=%f", i, square(i), i, dsquare(i));
+}
+
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                                                                                                                                                                                                                                   
+~                                           
